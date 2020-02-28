34ee7ff9f3335fabe5c87ced804adeb1523fde18
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-02-28 09:50:22 -0300

    Spring Security - Configurando o Repository e o Service

---
+++ src/main/java/com/api/rest/model/Role.java
@@ -0,0 +1,39 @@
+package com.api.rest.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+@SequenceGenerator(name = "seq_role", sequenceName = "seq_role", allocationSize = 1, initialValue = 1)
+public class Role implements GrantedAuthority {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_role")
+    private Long id;
+
+    private String nomeRole; /* Exemplo ROLE_SECRETARIO, ROLE_ADMIN */
+
+    @Override
+    public String getAuthority() { /* Returna o nome no Acesso ROLE_SECRETARIO, ROLE_ADMIN etc */
+        return this.nomeRole;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeRole() {
+        return nomeRole;
+    }
+
+    public void setNomeRole(String nomeRole) {
+        this.nomeRole = nomeRole;
+    }
+}
---
+++ src/main/java/com/api/rest/service/ImplementacaoUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.api.rest.service;
+
+import com.api.rest.model.Usuario;
+import com.api.rest.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImplementacaoUserDetailsService implements UserDetailsService {
+
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        /* Consultar no banco o usuario */
+        Usuario usuario = usuarioRepository.findUserByLogin(username);
+
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuário não encontrado");
+        }
+
+        return new User(usuario.getLogin(), usuario.getSenha(), usuario.getAuthorities());
+    }
+}

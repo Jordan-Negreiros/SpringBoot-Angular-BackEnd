92eff6884a9baccc7130fe6f2533e0178740c584
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-02-28 10:29:26 -0300

    Spring Security - Configurando Spring Security

---
+++ src/main/java/com/api/rest/security/WebConfigSecurity.java
@@ -0,0 +1,52 @@
+package com.api.rest.security;
+
+import com.api.rest.service.ImplementacaoUserDetailsService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+/* Mapeia URL, endereços, autoriza ou bloqueia acesso a URL */
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+
+    private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+
+    /* Configura as solicitações de acesso por Http */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        /* Ativando a proteção contra usuário que não estão validados por token */
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+
+        /* Ativando a restrição a URL, Permitindo Acesso a página inicial do sistema */
+        .disable().authorizeRequests().antMatchers("/").permitAll()
+        .antMatchers("/index").permitAll()
+
+        /* URL de Logout - Redireciona após o user deslogar do sistema */
+        .anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+
+        /* Mapeia URL de logout e invalida o usuário */
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+
+        /* Filtra as requisições de login para autenticação */
+
+        /* Filtra demais requisições para verificar a presença do TOKEN JWT no HEADER HTTP */
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        /* Service que irá consultar o usário no banco de dados */
+        auth.userDetailsService(implementacaoUserDetailsService)
+
+        /* Padrão de codificação de senha */
+        .passwordEncoder(new BCryptPasswordEncoder());
+
+    }
+}
